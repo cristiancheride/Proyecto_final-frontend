c2487d180335a4b641526989afbc006982bd0623
Cristian Cheride <crismoonchild@gmail.com>
2023-08-25 21:52:06 -0300

    Proyecto final - Primeros pasos del desarrollo frontend

---
+++ js/function.js
@@ -0,0 +1,32 @@
+
+document.getElementById('boton1').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("formacion").innerHTML = "<h4>Nivel Secundario: </h4><p>Bachiller con Orientación en Cs. Sociales y Humanidades (Finalizado) Programa de Educación a distancia Adultos 2000 (NES) RES. Nº 1536/98 - RES. Nº 1386/03- RES. N° 86/15- RES. N° 3558/17</p>";
+}
+
+document.getElementById('boton2').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("formacioncompl").innerHTML = "<h4>Operador Office (Centro de Formación Profesional Emiliano Emilio Calvo): </h4><p>Detalle: Paquete de Office 97, 2000, 2003</p> <h4>Diseñador Gráfico nivel 2 (Centro de Formación Profesional Emiliano Emilio Calvo): </h4><p>Detalle: Corel Draw, Paint Shop Pro, Photoshop</p> <h4>Operador de PC (Centro de Formación Profesional Emiliano Emilio Calvo): </h4><p>Detalle: Manejo de sistema Operativo</p> <h4>Reparador de PC (Centro de Formación Profesional Emiliano Emilio Calvo): </h4><p>Detalle: Armado y reparación de PC</p> <h4>Administrador de Redes (Centro de Capacitación Prored): </h4><p>Detalle: Administración de redes bajo servidores MS Windows NT4, Server 2000 y Server 2003 <h4>Secretariado Administrativo Contable (Centro Integral Lomas): </h4><p>Detalle: Tango Gestión Evolution</p>";
+}
+
+document.getElementById('boton3').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("tecnologia").innerHTML = "<h4> Diseño Gráfico Intermedio Photoshop - CorelDraw </h4><h4> S.O. Redes Intermedio Windows 2000, 2003 Server </h4><h4> Base de Datos Intermedio Access, Mysql, Sqlserver </h4><h4> Lenguajes Programación Intermedio Visual Basic, Intermedio, HTML y Php (Diseño Web) </h4><h4> ERP/CRM Intermedio Tango </h4><h4> Redes/Telefonía Intermedio TCP/IP </h4><h4> Otros Avanzado Windows, Internet, Email - Microsoft Office </h4><h4> Intermedio Seguridad Informática </h4><h4> Intermedio Reparación de PC </h4>";
+}
+
+
+document.getElementById('boton4').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("experiencia").innerHTML = "<h4>Municipalidad de Las Lomitas FSA (Administración – Sistemas)</h4><p>Posición: Sub Director General de Organización y Métodos </p> <p> Unidad especializada y de asesoramiento a la alta gerencia, cuya finalidad consiste en definir y aumentar la eficiencia, además rentabilidad de los sistemas administrativos y operativos, mediante el estudio, investigación, análisis, diseño, implantación y control de la organización de la Empresa, principalmente en cuanto a sus estructuras organizativas, objetivos, políticas, funciones y procedimientos. </p><p>12/2022 – 05/2023</p>  <h4>Municipalidad de Las Lomitas FSA (Soporte informático – Tecnología)</h4><p>Posición: Soporte en puestos de trabajo – Implementación de nuevas tecnologías y mejoras en las metodologías de trabajo. </p><p>05/2022 – 11/2022</p>   <h4>Ciriux (Soporte informático – Tecnología)</h4><p>Posición: Atención al público – Soporte online – Soporte presencial </p><p>Atención al público – Soporte de manera tanto online como presencial en empresa dedicada a Servicios de Impresión como venta, alquiler y soporte a empresas (mayoritariamente) y particulares. </p><p>También responsable de anuncios de venta en plataformas online como Mercadolibre y OLX entre otras. Manejo de proveedores </p><p>08/2014 – 09/2020</p><h4>Institutos MAS (Informática/Tecnología)</h4><p>Posición: Instructor </p><p> Área: Educación </p><p> SubArea: Docencia </p><p>Tipo de Puesto: Instructor </p><p>Tareas Realizadas: Windows XP, MS Office 2007, Tango Gestión Evolution, Corel Draw12, Adobe Photoshop CS2, Visual Basic. </p><p>02/2010 - 11/2012</p><h4> Tecnológico del Sur (Informática/Tecnología)</h4><p>Posición: Asistente Pedagógico/ Asesor en Ventas </p><p> Área: IT/Tecnología </p><p> Tipo de Puesto: Asesor </p><p>Tareas Realizadas: Telemarketing, Asesoría y venta de Cursos </p><p>11/2009 - 02/2010</p><h4>ProRed (Informática/Tecnología)</h4><p>Posición: Instructor </p><p> Área: Educación </p><p> SubArea: Docencia </p><p> Tipo de Puesto: Instructor </p><p> Tareas Realizadas: Aplicaciones MS Office, Redes Informáticas, Sistema Tango Gestión Contable, Diseño Gráfico. </p><p>03/2009 - 11/2009</p><h4>PC Zone (Informática/Tecnología)</h4><p>Posición: Atención al cliente </p><p> Área: IT/Tecnología </p><p>SubArea: Soporte Técnico/ Help Desk </p><p> Tipo de Puesto: Técnico </p><p>Tareas Realizadas: Atención al público, Reparaciones de equipos, armados de máquinas, Venta de insumos informáticos, armado de presupuestos, asistencia a domicilio y telefónica. </p><p>08/2007 - 12/2008</p>";
+}
+
+
+document.getElementById('boton5').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("objetivos").innerHTML = "<p>Si hay algo que me caracteriza es la capacidad de resolver problemas, puedo crear soluciones y optimizar las metodologías de trabajo. Esto combinado con los años de aprendizaje y experiencia obtenidas en cuanto a los sistemas y a las tecnologías de la información y a una gran capacidad creativa me vuelven una persona realmente proactiva y apasionada por aquello que decida llevar adelante.</p><p>Generar más en menos tiempo con menos costos y menos esfuerzo.</p>";
+}
+
+document.getElementById('boton6').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("referencias").innerHTML = "<h4>Juan Moreno </h4><p>0221154093407 (Jefe de Franquicia, Institutos MAS Florencio Varela)</p><h4>Mariano Pavoni</h4><p> 42448826 (Supervisor de área tecnológica, Centro Integral Lomas)</p><h4>Nicolas Francisco Gallo</h4><p> 9 11 2654-1514 (Dueño Ciriux)</p>";
+}
